242535190ac8dbcc67c086c3ed7874045befc9b5
Kellinton <kellintonbrasil@gmail.com>
2024-09-21 13:42:19 -0300

    feat: função para carregar exemplos de arquivos (xml e/ou json)

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     //#region Atualiza números de linhas
+
         const jsonInput = document.getElementById('jsonInput');
         const jsonLineNumbers = document.getElementById('jsonLineNumbers');
 
@@ -22,6 +23,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     //#endregion Atualiza números de linhas
 
-    //#region 
-    //#endregion
+    //#region Função para carregar exemplos de arquivos (xml e json)
+
+        // Botões de exemplo
+        document.getElementById('jsonExample').addEventListener('click', () => {
+            loadExample('/src/data/example.json', (data) => {
+                jsonInput.value = data;
+                updateLineNumbers(jsonInput, jsonLineNumbers);
+            });
+        });
+
+        document.getElementById('xmlExample').addEventListener('click', () => {
+            loadExample('/src/data/example.xml', (data) => {
+                xmlInput.value = data;
+                updateLineNumbers(xmlInput, xmlLineNumbers);
+            });
+        });
+
+
+        function loadExample(file, callback) {
+            fetch(file)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erro ao encontrar o arquivo');
+                    }
+                    return response.text();
+                })
+                .then(data => callback(data))
+                .catch(error => alert('Erro ao carregar o exemplo: ' + error));
+        }
+
+    //#endregion Função para carregar exemplos de arquivos (xml e json)
+
 })

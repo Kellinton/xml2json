c175c550d53004f94cd6c941b40cfc1cd5436171
Kellinton <kellintonbrasil@gmail.com>
2024-09-21 14:02:32 -0300

    feat: função para converter XML para JSON

--- src/js/script.js
+++ src/js/script.js
@@ -55,4 +55,100 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //#endregion Função para carregar exemplos de arquivos (xml e json)
 
+    //#region Função para converter XML em JSON
+        
+
+        document.getElementById('convertXmlToJson').addEventListener('click', () => {
+            const xmlText = xmlInput.value;
+
+
+            if (xmlText.trim() === '') {
+                alert('Por favor, insira um XML válido.');
+                return;
+            }
+
+            try {
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+
+
+                const parseError = xmlDoc.getElementsByTagName("parsererror");
+                if (parseError.length) {
+                    alert('Erro ao converter XML. Verifique se o XML está bem formado.');
+                    return;
+                }
+
+                const json = xmlToJson(xmlDoc);
+                jsonInput.value = JSON.stringify(json, null, 2);
+                updateLineNumbers(jsonInput, jsonLineNumbers);
+            } catch (e) {
+                alert('Erro ao converter XML para JSON.');
+            }
+        });
+
+        // Função para converter XML para JSON
+        function xmlToJson(xml) {
+            let obj = {};
+
+            if (xml.nodeType === 1) { // Elemento
+
+                if (xml.attributes.length > 0) {
+                    obj["@attributes"] = {};
+                    for (let j = 0; j < xml.attributes.length; j++) {
+                        const attribute = xml.attributes.item(j);
+                        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+                    }
+                }
+            } else if (xml.nodeType === 3) { // Texto
+                const value = xml.nodeValue.trim();
+                if (value) {
+                    return value; 
+                }
+            }
+
+            if (xml.hasChildNodes()) {
+                for (let i = 0; i < xml.childNodes.length; i++) {
+                    const item = xml.childNodes.item(i);
+                    const nodeName = item.nodeName;
+
+                    if (item.nodeType !== 3 || item.nodeValue.trim() !== '') {
+                        const childObj = xmlToJson(item);
+                        if (typeof (obj[nodeName]) === "undefined") {
+                            obj[nodeName] = childObj;
+                        } else {
+                            if (typeof (obj[nodeName]) === "object") {
+                                if (Array.isArray(obj[nodeName])) {
+                                    obj[nodeName].push(childObj);
+                                } else {
+                                    obj[nodeName] = [obj[nodeName]];
+                                    obj[nodeName].push(childObj);
+                                }
+                            } else {
+                                obj[nodeName] = [obj[nodeName], childObj];
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            for (let key in obj) {
+                if (typeof obj[key] === "object" && obj[key] !== null) {
+                    if (obj[key].hasOwnProperty("#text")) {
+                        obj[key] = obj[key]["#text"];
+                    } else {
+                        for (let subKey in obj[key]) {
+                            if (typeof obj[key][subKey] === "object" && obj[key][subKey].hasOwnProperty("#text")) {
+                                obj[key][subKey] = obj[key][subKey]["#text"];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return obj;
+        }
+
+    //#endregion Função para converter XML em JSON
+
 })
